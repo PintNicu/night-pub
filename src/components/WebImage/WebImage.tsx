4dfa7b634b1681c363e93ac1f22d9891114c08cf
Nicup <nicup.2023@gmail.com>
2024-09-07 21:22:08 +0300

    Updates to icons and more

--- src/components/WebImage/WebImage.tsx
+++ src/components/WebImage/WebImage.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import cocktailsPouring from '../Images/GalleryImages/316430565_678047767365026_8113960512345367173_n.jpg';
 
-const WebImage: React.FC = () => {
+const ImageDisplay: React.FC = () => {
+    const imagePath = require('../../Images/GalleryImages/316430565_678047767365026_8113960512345367173_n.jpg');
+
     return (
         <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <img
-                src={cocktailsPouring}
-                alt="Zepelin 1929 cocktails"
+                src={imagePath}
+                alt="Zepelin 1929 Interior"
                 style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
             />
         </div>
     );
 };
 
-export default WebImage;
+export default ImageDisplay;
